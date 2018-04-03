8ea5b347cda5c913b63ba1fb4a1c34016a3877d8
Lim Chee Aun <cheeaun@gmail.com>
2018-04-03 22:12:36 +0800

    Pick exact turf methods

--- assets/turf-modules.js
+++ assets/turf-modules.js
@@ -1,7 +1,11 @@
 var helpers = require('@turf/helpers');
 
-module.exports = Object.assign({
+module.exports = {
   pointsWithinPolygon: require('@turf/points-within-polygon'),
   nearestPoint: require('@turf/nearest-point'),
   circle: require('@turf/circle'),
-}, helpers);
+  multiPoint: helpers.multiPoint,
+  point: helpers.point,
+  points: helpers.points,
+  polygon: helpers.polygon,
+};
--- assets/turf.js
+++ assets/turf.js
@@ -1,11 +1,15 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.turf = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 var helpers = require('@turf/helpers');
 
-module.exports = Object.assign({
+module.exports = {
   pointsWithinPolygon: require('@turf/points-within-polygon'),
   nearestPoint: require('@turf/nearest-point'),
   circle: require('@turf/circle'),
-}, helpers);
+  multiPoint: helpers.multiPoint,
+  point: helpers.point,
+  points: helpers.points,
+  polygon: helpers.polygon,
+};
 },{"@turf/circle":3,"@turf/helpers":10,"@turf/nearest-point":15,"@turf/points-within-polygon":16}],2:[function(require,module,exports){
 'use strict';
 
