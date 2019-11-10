98c7bdfb65ad7c1affd338b75e64a3fb2117ac23
Lim Chee Aun <cheeaun@gmail.com>
2019-11-10 18:38:01 +0800

    Fix turf modules not working

--- assets/turf-modules.js
+++ assets/turf-modules.js
@@ -1,9 +1,9 @@
 var helpers = require('@turf/helpers');
 
 module.exports = {
-  pointsWithinPolygon: require('@turf/points-within-polygon'),
-  nearestPoint: require('@turf/nearest-point'),
-  circle: require('@turf/circle'),
+  pointsWithinPolygon: require('@turf/points-within-polygon').default,
+  nearestPoint: require('@turf/nearest-point').default,
+  circle: require('@turf/circle').default,
   multiPoint: helpers.multiPoint,
   point: helpers.point,
   points: helpers.points,
